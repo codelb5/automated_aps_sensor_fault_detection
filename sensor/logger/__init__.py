8229c59a78371baa30b8b4c03165455b19ab062a
lbhaarathi <lakshmibhaarathiofcl@gmail.com>
2023-04-05 17:57:37 +0530

    final changes

--- sensor/logger/__init__.py
+++ sensor/logger/__init__.py
@@ -1,11 +1,10 @@
 import os
 import logging
 from datetime import datetime
-from from_root import from_root
 
 LOG_FILE_NAME = f"{datetime.now().strftime('%d%m%Y_%H%M%S')}.log"
 
-LOG_DIR = os.path.join(from_root(),"logs",LOG_FILE_NAME)
+LOG_DIR = os.path.join("logs",LOG_FILE_NAME)
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
